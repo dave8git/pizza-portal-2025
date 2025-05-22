1b3c9ac8ec7d8abd6f37ca9a05f68c62abb274cd
dave8git <dave8git@gmail.com>
2025-05-22 21:27:01 +0200

    Add boundaries from 1 - 10 on newValue set

--- dist/js/script.js
+++ dist/js/script.js
@@ -178,28 +178,30 @@ const select = {
       const thisWidget = this;
       const newValue = parseInt(value);
       if(thisWidget.value !== newValue && !isNaN(newValue)) {
-          thisWidget.value = newValue;
-      }
+        if(newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax) {
+           thisWidget.value = newValue;
+        }
+      } 
       thisWidget.input.value = thisWidget.value;
     }
     initActions(){
-  const thisWidget = this;
+    const thisWidget = this;
 
-  thisWidget.input.addEventListener('change', () => {
-    thisWidget.setValue(thisWidget.input.value);
-  });
+    thisWidget.input.addEventListener('change', () => {
+      thisWidget.setValue(thisWidget.input.value);
+    });
 
-  thisWidget.linkDecrease.addEventListener('click', (event) => {
-    event.preventDefault();
-    thisWidget.setValue(thisWidget.value - 1);
-  });
+    thisWidget.linkDecrease.addEventListener('click', (event) => {
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value - 1);
+    });
 
-  thisWidget.linkIncrease.addEventListener('click', (event) => {
-    event.preventDefault();
-    thisWidget.setValue(thisWidget.value + 1);
-  });
-}
+    thisWidget.linkIncrease.addEventListener('click', (event) => {
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value + 1);
+    });
   }
+}
 
   const app = {
     initMenu: function() {
