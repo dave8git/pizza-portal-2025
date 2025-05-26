32b649dd04aafbbb22efdcfa1bf633d8c52073a7
dave8git <dave8git@gmail.com>
2025-05-26 11:02:25 +0200

    Add cart opens

--- src/js/script.js
+++ src/js/script.js
@@ -242,20 +242,26 @@ const select = {
 class Cart{
   constructor(element) {
     const thisCart = this;
-
     thisCart.products = [];
-
     thisCart.getElements(element);
+    thisCart.initActions();
 
     console.log('new Cart', thisCart);
   }
 
   getElements(element) {
     const thisCart = this;
-
     thisCart.dom = {};
-
     thisCart.dom.wrapper = element;
+    thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+  }
+
+  initActions() {
+    const thisCart = this;
+    thisCart.dom.toggleTrigger.addEventListener('click', function(event) {
+      event.preventDefault();
+      thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+    });
   }
 }
 
@@ -275,6 +281,13 @@ class Cart{
       thisApp.data = dataSource;
     },
 
+    initCart: function() {
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -284,7 +297,8 @@ class Cart{
       console.log('templates:', templates);
 
       thisApp.initData();
-      thisApp.initMenu(); 
+      thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
