8997c0ffa1e6960efe505cf37eb2cb17dfe195c3
dave8git <dave8git@gmail.com>
2025-07-01 13:09:23 +0200

    Add started working on windows.location.hash

--- src/js/app.js
+++ src/js/app.js
@@ -7,12 +7,45 @@ const app = { // now divide into subclasses
       const thisApp = this;
 
       thisApp.pages = document.querySelector(select.containerOf.pages).children;
-
+      thisApp.navLinks = document.querySelectorAll(select.nav.links);
       thisApp.activatePage(thisApp.pages[0].id);
+
+      for (let link of thisApp.navLinks) {
+        link.addEventListener('click', function(event){
+          const clickedElement = this;
+          event.preventDefault();
+
+          /* get page id from href attribute */
+          const id = clickedElement.getAttribute('href').replace('#', '');
+
+          /* run thisApp.activePage with that id */
+          thisApp.activatePage(id);
+
+          /* change URL hash */
+          window.location.hash = '#/' + id;
+        });
+      }
     },
 
     activatePage: function(pageId) {
       const thisApp = this;
+
+      /* add class "active" to matching pages, remove from non-matching */
+      for(let page of thisApp.pages) {
+        // if(page.id = pageId) {
+        //   page.classList.add(classNames.pages.active);
+        // } else {
+        //   page.classList.remove(classNames.pages.active);
+        // }
+        page.classList.toggle(classNames.pages.active, page.id == pageId);
+      }
+      /* add class "active" to matching links, remove from non-matching */
+      for(let link of thisApp.navLinks) {
+        link.classList.toggle(
+          classNames.nav.active,
+          link.getAttribute('href') == '#' + pageId
+        );
+      }
     },
 
     initMenu: function() {
