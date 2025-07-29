a923a5db37dff9fe5cf33462ade492db371af226
dave8git <dave8git@gmail.com>
2025-07-29 20:58:33 +0200

    Add updateSliderBackground

--- dist/js/components/HourPicker.js
+++ dist/js/components/HourPicker.js
@@ -15,11 +15,13 @@ class HourPicker extends BaseWidget {
 
   updateSliderBackground() {
     const thisWidget = this;
-    const bookingInstance = window.bookingInstance;
-    if(!bookingInstance) return;
+    const bookingInstance = window.bookingInstance; // Make sure it's globally accessible
+    if (!bookingInstance) return;
+
     const gradient = bookingInstance.generateSliderColorSegments();
     thisWidget.dom.input.style.background = `linear-gradient(to right, ${gradient})`;
   }
+
   initPlugin() {
     const thisWidget = this;
     const slider = thisWidget.dom.input;
@@ -41,7 +43,7 @@ class HourPicker extends BaseWidget {
       connect: [true, false],
       tooltips: true,
       format: {
-        to: value =>  utils.numberToHour(value),//value.toFixed(2),
+        to: value => utils.numberToHour(value),//value.toFixed(2),
         from: value => parseFloat(value),
       },
     });
@@ -64,7 +66,7 @@ class HourPicker extends BaseWidget {
   }
 
   renderValue() {
-     const thisWidget = this;
+    const thisWidget = this;
     if (thisWidget.dom.output) {
       thisWidget.dom.output.innerHTML = utils.numberToHour(thisWidget.value); // show "12:30"
       // console.log(thisWidget.value);
